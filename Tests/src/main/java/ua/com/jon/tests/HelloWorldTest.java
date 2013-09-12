7403289b168127e787988c657d3b10d4353203aa
alex-zr <oleksandr.roshchupkin@gmail.com>
2013-09-12 08:26:19 +0300

    hello test

--- Tests/src/main/java/ua/com/jon/tests/HelloWorldTest.java
+++ Tests/src/main/java/ua/com/jon/tests/HelloWorldTest.java
@@ -1,9 +1,14 @@
 package ua.com.jon.tests;
 
+import com.jon.tron.service.evaluation.EvaluationUtil;
 import com.jon.tron.service.junit.Unit;
 import com.jon.tron.service.reflect.ReflectionUtil;
+import org.apache.log4j.Logger;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.*;
 
 import static junit.framework.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
@@ -15,21 +20,33 @@ import static org.junit.Assert.assertTrue;
  */
 @Unit(testName = "Hello world", value = "weekend1.task1")
 public class HelloWorldTest {
+    private static final Logger log = Logger.getLogger(HelloWorldTest.class);
 
     @Unit
     private static Class unitClass;
 
+//    private Reader in;
+    private PrintStream out;
+    EvaluationUtil evaluationUtil;
     private Object instance;
 
     @Before
     public void setUp() {
         try {
+            evaluationUtil = new EvaluationUtil();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            evaluationUtil.setInOut("", baos);
             instance = unitClass.newInstance();
         } catch (InstantiationException e) {
             e.printStackTrace();
         } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
+    }
+
+    @After
+    public void tearDown() {
+
     }
 
     @Test
