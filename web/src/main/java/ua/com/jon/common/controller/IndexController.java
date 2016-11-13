2f1a94027867a6541e006374668e57c71b6ebea6
alex-zr <oleksandr.roshchupkin@gmail.com>
2016-11-13 10:00:46 +0200

    switch to index

--- web/src/main/java/ua/com/jon/common/controller/IndexController.java
+++ web/src/main/java/ua/com/jon/common/controller/IndexController.java
@@ -52,7 +52,7 @@ public class IndexController {
     @RequestMapping("/")
     public String landing(ModelMap modelMap)  {
         modelMap.put("item","item9");
-        return "indexSpa";
+        return "index";
     }
 
 /*    @ExceptionHandler(ResourceNotFoundException.class)
